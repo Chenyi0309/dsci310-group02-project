0fdb9799923c9108bbf14c18441bda103a179d14
Prabhjot Singh <100841429+prabh35@users.noreply.github.com>
2024-03-14 14:23:23 -0700

    Update data_preprocessing.R
    created a results directory and made a table to show cleaned data

--- scripts/data_preprocessing.R
+++ scripts/data_preprocessing.R
@@ -11,3 +11,11 @@ housing_data <- select(housing_data, -url, -id, -Cid) %>%
 
 # Check for missing values
 summary(housing_data)
+
+# Create a 'results' directory if it doesn't exist
+if (!dir.exists("results")) {
+  dir.create("results")
+}
+
+# Generate a table of the clean data to be used in the report
+write.csv(housing_data, "results/clean_data_table.csv", row.names = FALSE)
